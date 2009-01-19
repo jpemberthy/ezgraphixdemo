174019c87531ff5b9b83471938c95366b425b824
jpemberthy <jpemberthy@gmail.com>
2009-01-19 14:48:54 -0500

    column3d and pie3d

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,15 @@
 class ReportsController < ApplicationController
   
   def index
+    #creating a column3D chart.
+    @col3d = Ezgraphix::Graphic.new(:div_name => 'columnd3d', :c_type => 'col3d')
+    @col3d.data = {:usa => 306173000, :uk => 60975000, :brazil => 190467249, :russia => 142008838}
+    @col3d.render_options(:w => 400, :y_name => 'Population/Millions')
     
+    #creating a pie3D chart.
+    @pie3d = Ezgraphix::Graphic.new(:div_name => 'pie3d', :c_type => 'pie3d', :w => 400)
+    @pie3d.data = @col3d.data
+    @pie3d.render_options(:caption => 'Estimated population 2008')
   end
 
 end
